ec84cb627212a5051008101e57014e13af41b7b8
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-09-21 14:49:14 +0530

    feat: add profile dropdown and logout functionality to Navbar component

--- Job Portal/src/components/Navbar.jsx
+++ Job Portal/src/components/Navbar.jsx
@@ -1,11 +1,23 @@
 // import React from 'react'
-import { useContext } from "react";
-import { Link } from "react-router";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router";
 import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const [profile, setProfile] = useState(false);
+
   const { user } = useContext(AuthContext);
   console.log(user);
+
+  const showProfile = () => {
+    setProfile((prev) => !prev);
+  };
+
+  const logout = () => {
+    navigate("/login");
+  };
   return (
     <nav className="flex w-full items-center justify-between border-b border-slate-800 bg-slate-950 px-5 py-5 pt-10 pb-10 text-white sm:px-10">
       <Link to="/" className="text-lg font-bold tracking-tight sm:text-xl">
@@ -25,7 +37,10 @@ const Navbar = () => {
           >
             Job Form
           </Link>
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-red-500 font-bold text-white">
+          <div
+            onClick={showProfile}
+            className="flex h-10 w-10 items-center justify-center rounded-full bg-red-500 font-bold text-white"
+          >
             {!user.username ? (
               <img
                 className="rounded-full"
@@ -35,6 +50,19 @@ const Navbar = () => {
               user.username?.split("")[0]
             )}
           </div>
+          {profile && (
+            <div className="absolute right-0 mt-38 mr-8 w-32 bg-slate-600 rounded-lg shadow-lg overflow-hidden z-50">
+              <button className="w-full text-left px-4 py-3 text-white border-b hover:bg-red-500">
+                Profile
+              </button>
+              <button
+                onClick={logout}
+                className="w-full text-left px-4 py-3 text-white hover:bg-red-500"
+              >
+                Logout
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </nav>
